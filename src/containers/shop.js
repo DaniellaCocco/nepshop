eb3de0696663784cc9cb211ed091de11982ad296
Daniella <daniella810@gmail.com>
2016-12-04 16:55:50 +0100

    Basic grid

--- src/containers/shop.js
+++ src/containers/shop.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react'
 import MainMenu from '../components/main_menu'
 import Products from '../components/products'
 import Cart from '../containers/cart'
-import { Header } from 'semantic-ui-react'
+import { Grid, Header } from 'semantic-ui-react'
 
 class Shop extends Component {
   render() {
     return (
       <div className="shop">
         <MainMenu />
-        <Header as='h2'>Products</Header>
-        <Products />
-        <Header as='h2'>Cart</Header>
-        <Cart />
+        <Grid padded>
+          <Grid.Column>
+            <Header as='h2'>Products</Header>
+            <Products />
+            <Header as='h2'>Cart</Header>
+            <Cart />
+          </Grid.Column>
+        </Grid>
       </div>
     )
   }
