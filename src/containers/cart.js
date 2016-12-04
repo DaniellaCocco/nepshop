1dc1d5f0e5c7af961bf9eab9cc9f1bac30ef9248
Daniella <daniella810@gmail.com>
2016-12-04 23:31:23 +0100

    Remove unused imports

--- src/containers/cart.js
+++ src/containers/cart.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { getCartProducts,
-  getTotalCash,
-  getTotalNumber } from '../reducers/index.js'
 import { Item } from 'semantic-ui-react'
 
 let cart_items = []
