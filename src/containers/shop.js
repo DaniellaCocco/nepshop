35c1b1ba3324088f26f5e990e08a4b2171fd872a
Daniella <daniella810@gmail.com>
2016-12-04 12:14:34 +0100

    Basic HTML

--- src/containers/shop.js
+++ src/containers/shop.js
@@ -3,7 +3,27 @@ import React, { Component } from 'react'
 export default class Shop extends Component {
   render() {
     return (
-      <p>Hello World!</p>
+      <div className="shop">
+        <div className="menu">
+          <div className="item">Products</div>
+          <div className="item">Cart</div>
+        </div>
+        <div className="products">
+          <div className="product">
+            <p className="name">Fiat Panda</p>
+            <p className="year">2006</p>
+            <p className="color">Black</p>
+          </div>
+          <div className="product">
+            <p className="name">Fiat 500</p>
+            <p className="year">2010</p>
+            <p className="color">Red</p>
+          </div>
+        </div>
+        <div className="cart">
+          <p>Your cart is empty.</p>
+        </div>
+      </div>
     )
   }
 }
